683a37133289ce9f746b19027727f2c9d7fd5e2c
Hiaguedes <hiago@contasimples.com>
2021-09-15 15:42:48 -0300

    fazendo configuracao de cobertura de testes

--- jest.config.ts
+++ jest.config.ts
@@ -1,16 +1,26 @@
 // jest.config.ts
 // install ts-node to work
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 import type {Config} from '@jest/types';
 
 export default async (): Promise<Config.InitialOptions> => {
   return {
     verbose: true,
     preset: "jest-expo",
+    testEnvironment: 'node',
     transformIgnorePatterns: [
       "node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)"
     ],
     setupFilesAfterEnv: [
       'jest-styled-components',
+    ],
+    collectCoverage: true,
+    collectCoverageFrom: [
+      "src/**/*.tsx",
+      "!src/**/*.test.tsx"
+    ],
+    coverageReporters: [
+      "lcov"
     ]
   };
 };
